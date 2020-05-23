2d6d44d955e7bad7b58083391c5e4e1504aa0d4e
Kate W <kate99w@gmail.com>
2020-05-23 08:42:13 -0700

    add spring-kate99w module

---
+++ spring-kate99w/src/main/java/com/kate99w/MyTestBean.java
@@ -0,0 +1,33 @@
+package com.kate99w;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class MyTestBean implements InitializingBean, ApplicationContextAware {
+
+    /**
+     * 构造函数
+     */
+    public MyTestBean(){
+        System.out.println("MyTestBean 构造器...");
+    }
+
+
+    /**
+     * InitializingBean 接口实现
+     */
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("MyTestBean afterPropertiesSet...");
+    }
+
+    public void print() {
+        System.out.println("print方法业务逻辑执行");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("setApplicationContext....");
+    }
+}
